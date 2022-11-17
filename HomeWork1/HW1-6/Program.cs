d92e1bec7dcaf5a146d321e52f09ced0f20dd282
Artem <artuom-paul@mail.ru>
2022-11-17 23:07:59 +0800

    HW

---
+++ HomeWork1/HW1-6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0) 
+{
+   Console.WriteLine("четное число"); 
+}
+else
+{
+   Console.WriteLine("не четное число"); 
+}
+
---
+++ HomeWork1/HW1-8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число N: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine($"Все четные числа от 1 до {num1}: ");
+
+while (i <= num1)
+
+{
+    if (i % 2 == 0)
+    Console.WriteLine(i);
+    i++;
+}
+
+
+
+
+
+
