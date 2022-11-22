bcabf83b17ffed61ab57bd502b44c34e59d76ce7
Artem <artuom-paul@mail.ru>
2022-11-22 23:29:47 +0800

    задание 19

--- HomeWork3/3-19/Program.cs
+++ HomeWork3/3-19/Program.cs
@@ -12,63 +12,21 @@ if (num1 > 9999 && num1 < 100000)
 {
     int num2 = num1 % 10;
     int num3 = num1 / 10000;
-    if (num2 == num3 )
+    int num4 = num1 / 10 % 10; 
+    int num5 = num1 / 1000 % 10;
+    if (num2 == num3 && num4 == num5)
     {
-        int num4 = num1 / 10 % 10; 
-        int num5 = num1 / 1000 % 10;
-    
-        if (num4 == num5)
-        {
-            Console.WriteLine("Это число палиндром");
-        }
-        else 
-        {
-            Console.WriteLine("Это число не палиндром");
-        } 
+       Console.WriteLine("Это число палиндром"); 
     }
     else 
     {
             Console.WriteLine("Это число не палиндром");
-    }
+    } 
 }
 else
 {
     Console.WriteLine("Это число не пятизначное");
 }
-
-    
-
-// Console.WriteLine("Введите пятизначное число: ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-
-
-// if (num1 > 9999 && num1 < 100000)
-// {
-//     int num2 = num1 % 10;
-//     int num3 = num1 / 10000;
-//     if (num2 == num3)
-//     {
-//         int num4 = num1 / 10 % 10; 
-//         int num5 = num1 / 1000 % 10;
-    
-//         if (num4 == num5)
-//         {
-//             Console.WriteLine("Это число палиндром");
-//         }
-//         else 
-//         {
-//             Console.WriteLine("Это число не палиндром");
-//         } 
-//     }
-//     else 
-//     {
-//             Console.WriteLine("Это число не палиндром");
-//     }
-// }
-// else
-// {
-//     Console.WriteLine("Это число не пятизначное");
-// }
 
     
 
