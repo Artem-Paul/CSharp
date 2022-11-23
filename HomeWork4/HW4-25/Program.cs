d370a3fe7cea762da5437b245dcd1324b8906e5a
Artem <artuom-paul@mail.ru>
2022-11-24 01:24:28 +0800

    задание 25

---
+++ HomeWork4/HW4-25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {num1} в степени {num2}: " + GetSquare(num1));
+
+int GetSquare(int num)
+{
+    int result = num;
+    for (int i = 1; i < num2; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+
+
