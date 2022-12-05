561f80dbb50dd797b11850dccf043d1937aae3a6
Artem <artuom-paul@mail.ru>
2022-12-05 10:30:39 +0800

    ArrayExtension

--- LSN6/003/ArrayExtension.cs
+++ LSN6/003/ArrayExtension.cs
@@ -1,16 +1,16 @@
-namespace task3
-{
-    public static class ArrayExtension
-    {
-        public static int[] CopyArray(this int[])
-        {
-            int[] arrayCopy = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-            {
-                arrayCopy[i] = array[i];
-            }
+// namespace task3
+// {
+//     public static class ArrayExtension
+//     {
+//         public static int[] CopyArray(this int[])
+//         {
+//             int[] arrayCopy = new int[array.Length];
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 arrayCopy[i] = array[i];
+//             }
 
-            return arrayCopy;
-        }
-    }
-}
+//             return arrayCopy;
+//         }
+//     }
+// }
