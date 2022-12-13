050c17305f70e43c5b1058208ce5e210cecb85b5
Artem <artuom-paul@mail.ru>
2022-12-13 10:13:33 +0800

    задание 62

--- HomeWork8/8-62/Program.cs
+++ HomeWork8/8-62/Program.cs
@@ -5,21 +5,47 @@
 // 11 16 15 06
 // 10 09 08 07
 
-var arr = FillMatrix(4, 4);
+var arr = SpiralMatrix(4, 4);
 PrintMatrix(arr);
 
-int[,] FillMatrix(int rows, int colums)
+int[,] SpiralMatrix(int rows, int colums)
 {
     int[,] matrix = new int[rows, colums];
-    int k = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int value = 1;
+    int i = 0;
+    int j = -1;
+
+    while (++j < rows)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = k + 1;
-            k++;
-        }
+        matrix[0, j] = value;
+        value++;
     }
+    while (++i < colums)
+    {
+        matrix[i, colums - 1] = value;
+        value++;
+    }
+    while (--j > 0)
+    {
+        matrix[rows - 1, j - 1] = value;
+        value++;
+    }
+    while (--i > 1)
+    {
+        matrix[i - 1, 0] = value;
+        value++;
+    }
+    while (j++ < rows - 2)
+    {
+        matrix[1, j] = value;
+        value++;
+    }
+    while (i++ < colums - 2)
+    {
+        matrix[i, colums - 2] = value;
+        value++;
+    }
+    matrix[2, 1] = value;
 
     return matrix;
 }
@@ -33,11 +59,11 @@ void PrintMatrix(int[,] matrix)
             {
                 if (matrix[i, j] > 9)
                 {
-                Console.Write($"{matrix[i, j]} ");
+                    Console.Write($"{matrix[i, j]} ");
                 }
                 if (matrix[i, j] <= 9)
                 {
-                Console.Write($"0{matrix[i, j]} ");
+                    Console.Write($"0{matrix[i, j]} ");
                 }
             }
         }
